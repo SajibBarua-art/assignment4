03b0b243c576b5494d383619ca2c70ec887d3c85
SajibBarua-art <sajibbaruanstu@gmail.com>
2021-02-07 18:12:17 +0600

    meals

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
-const mealDetails = (mealData) => {
+const mealDetails = mealData => {
+    const mealImage = mealData.strMealThumb;
     const mealDescription = mealData.strMeal;
-    const mealImages = mealData.strMealThumb;
-    console.log(mealDescription, mealImages);
+    // console.log(mealImages);
+
+    const relatedMeals = document.getElementById('relatedMeals');
+    const div = document.createElement('div');
+    div.className = 'mealInfo'
+    const mealInformation = `
+        <img class="mealImage" src="${mealImage}">
+        <h3 class="mealName">${mealDescription}</h3>
+    `
+    div.innerHTML = mealInformation;
+    relatedMeals.appendChild(div);
 }
-const allMealsDetails = (searchMealCategory) => {
-    console.log(searchMealCategory);
+const allMealsDetails = searchMealCategory => {
     fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c="+searchMealCategory)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
+             //console.log(data);
             const allMeals = data.meals;
-            for (let i = 0; i < allMeals.length; i++) {
-                const mealData = allMeals[i];
-                mealDetails(mealData);
-            }
+            allMeals.map((mealData) => mealDetails(mealData));
         })
 }
 const main = () => {
-    const mealCategoryId = document.getElementById('mealCategory');
+    const mealCategoryId = document.getElementById('searchBox');
     const searchButtonId = document.getElementById('searchButton');
     searchButtonId.addEventListener('click', () => {
         allMealsDetails(mealCategoryId.value);
